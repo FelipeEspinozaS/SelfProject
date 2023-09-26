b9c3f2a1018c6d4527424e56b4b8a47bc1867bfa
Felipe Espinoza <felipe.espinozas@uc.cl>
2023-09-26 15:54:19 -0300

    Device

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root 'hello_world#hello'
 
   get 'hello_world/hello', to: 'hello_world#hello'
+
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, 
+                   path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  #FALTA VER BIEN LO DE LA RUTAA
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
